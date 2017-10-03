10c83a2134b9f9c2b34db768d9164d049fc531a6
DESKTOP-DQU9OUJ\Javi <jmerinocoll@gmail.com>
2017-10-03 13:36:47 +0200

    Subida de primera actividad

---
+++ JsAdivinacion/Adivinacion.js
@@ -0,0 +1,12 @@
+var num1 = 52;
+var num2 = 0;
+
+num2 =  prompt ("Di un número entre 1 y 100");
+
+if (num2 > num1){
+  alert ("TE PASASTE");
+}else if (num2 < num1){
+  alert ("TE QUEDASTE CORTO");
+}else {
+  alert ("ACERTASTE");
+}
