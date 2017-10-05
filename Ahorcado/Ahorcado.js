5d6c5f896c51dfc37b3a8f103011299454db2060
DESKTOP-DQU9OUJ\Javi <jmerinocoll@gmail.com>
2017-10-05 10:14:16 +0200

    Actividad "el ahorcado"

---
+++ Ahorcado/Ahorcado.js
@@ -0,0 +1,15 @@
+var letra1 = "c";
+var letra3 = "C";
+
+var letra2 = "";
+
+letra2 = prompt ("Di una letra" , "");
+
+
+if (letra1 == letra2){
+  alert ("¡Has acertado!");
+}if (letra3 == letra2){
+  alert ("Has acertado");
+}else if (letra1 != letra2){
+  alert ("¡Oh, has fallado!");
+}
