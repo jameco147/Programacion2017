e80577cd6d0134319401615f306c2d51377095d7
Javi <jmerinocoll@gmail.com>
2018-03-15 10:27:31 +0100

    Actividad 07

---
+++ 3Eval/Proyecto07/model/Consulta.php
@@ -0,0 +1,91 @@
+<?php
+require_once "Db.php";
+
+/**
+ *
+ */
+class Consulta
+{
+  private $db;
+  private $conector;
+
+  function __construct()
+  {
+  $this->db=new Db();
+  $this->db->conectar();
+  $this->conector=$this->db->getConector();
+  }
+
+
+  public function insertar()
+  {
+    if (empty($_POST['username']) || empty($_POST['userlastname']) || empty($_POST['age']) || empty($_POST['course']) || empty($_POST['score']) || empty($_POST['email']) ) {
+      echo "<br><p align = center>No dejes ningún campo vacío</p><br>";
+      echo "<p align = center><a href='InsertarUsuario.php'>Por favor vuelve a resgistrarte, gracias</a></p><br><br><br>";
+
+    } else {
+
+        $jugador = "SELECT * FROM juegos.usuarios WHERE nombre = '$_POST[username]'";
+        $comprobarJugador = $this->conector->query($jugador);
+        $contar = mysqli_num_rows($comprobarJugador);
+
+        if ($this->conector->connect_errno) {
+          echo "Fallo al conectar a MySQL: " .$conexion->connect_error;
+        } elseif ($contar == 1) {
+          echo "<br><p align = center>El nick ya ha sido utilizado</p><br>";
+          echo "<p align = center><a href='InsertarUsuario.php'>Por favor vuelve a resgistrarte con otro nombre</a></p><br><br><br>";
+        } else  {
+
+          $registro = "INSERT INTO juegos.usuarios (nombre, apellidos, edad, curso, puntuacion, correo) VALUES ('$_POST[username]', '$_POST[userlastname]', '$_POST[age]','$_POST[course]', '$_POST[score]', '$_POST[email]')";
+
+          if ($this->conector->query($registro) === TRUE) {
+            echo "<br><br><h1 align = center>Usuario creado correctamente</h1><br><br><br>";
+          }
+        }
+      }
+  }
+
+
+  public function actualizar()
+  {
+    if (empty($_POST['newusername']) || empty($_POST['newuserlastname']) || empty($_POST['newage']) || empty($_POST['newcourse']) || empty($_POST['newscore']) || empty($_POST['newemail'])) {
+      echo "<br><p align = center>No dejes ningún campo vacío</p><br>";
+      echo "<p align = center><a href='actualizarUsuario.php'>Por favor vuelve a intentarlo, gracias</a></p><br><br><br>";
+    } else {
+
+        $actualizar = "UPDATE usuarios SET nombre = '$_POST[newusername]', apellidos = '$_POST[newuserlastname]', edad = '$_POST[newage]', curso = '$_POST[newcourse]', puntuacion = '$_POST[newscore]', correo = '$_POST[newemail]' WHERE nombre = '$_POST[actu]'";
+
+        if ($this->conector->query($actualizar) === TRUE) {
+          echo "<br><br><h1 align = center>Usuario actualizado correctamente</h1><br><br><br>";
+        }
+      }
+  }
+
+  public function borrar()
+  {
+    if (empty($_POST['eliminar'])) {
+      echo "<br><p align = center>Error</p><br>";
+      echo "<p align = center><a href='borrarUsuario.php'>Selecciona un usuario para eliminar</a></p><br><br><br>";
+
+    }else {
+
+        $borrar = "DELETE FROM usuarios WHERE nombre = '$_POST[eliminar]'";
+
+
+        if ($this->conector->query($borrar) === TRUE) {
+          echo "<br><br><h1 align = center>Usuario eliminado</h1><br><br><br>";
+
+        }
+      }
+  }
+
+
+  public function getUsuarios()
+  {
+      $cons = $this->conector->query("SELECT nombre,apellidos,correo FROM usuarios");
+      return $cons;
+  }
+
+
+}
+?>
---
+++ 3Eval/Proyecto07/model/Db.php
@@ -0,0 +1,192 @@
+<?php
+require_once "config/config.php";
+
+/**
+ *
+ */
+class Db
+{
+  private $server="";
+  private $user="";
+  private $pass="";
+  private $db="";
+
+  private $conector;
+
+  function __construct()
+  {
+    global $config;
+    $this->server=$config["host"];
+    $this->user=$config["user"];
+    $this->pass=$config["pass"];
+    $this->db=$config["db"];
+  }
+
+  function conectar()
+  {
+    $conectorTmp = new mysqli ($this->server, $this->user, $this->pass, $this->db);
+
+    if ($conectorTmp->connect_errno) {
+      $this->conector=false;
+    }else{
+      $this->conector=$conectorTmp;
+    }
+  }
+
+  function desconectar()
+  {
+    //if ($this->conector->connect_errno) {
+      //echo "Ninguna conexion que cerrar";
+    //}else{
+      mysqli_close($this->conector);
+      echo "Conexión finalizada";
+  //  }
+  }
+
+    /**
+     * Get the value of Server
+     *
+     * @return mixed
+     */
+    public function getServer()
+    {
+        return $this->server;
+    }
+
+    /**
+     * Set the value of Server
+     *
+     * @param mixed server
+     *
+     * @return self
+     */
+    public function setServer($server)
+    {
+        $this->server = $server;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of User
+     *
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of User
+     *
+     * @param mixed user
+     *
+     * @return self
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Pass
+     *
+     * @return mixed
+     */
+    public function getPass()
+    {
+        return $this->pass;
+    }
+
+    /**
+     * Set the value of Pass
+     *
+     * @param mixed pass
+     *
+     * @return self
+     */
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Db
+     *
+     * @return mixed
+     */
+    public function getDb()
+    {
+        return $this->db;
+    }
+
+    /**
+     * Set the value of Db
+     *
+     * @param mixed db
+     *
+     * @return self
+     */
+    public function setDb($db)
+    {
+        $this->db = $db;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Conexion
+     *
+     * @return mixed
+     */
+    public function getConexion()
+    {
+        return $this->conexion;
+    }
+
+    /**
+     * Set the value of Conexion
+     *
+     * @param mixed conexion
+     *
+     * @return self
+     */
+    public function setConexion($conexion)
+    {
+        $this->conexion = $conexion;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the value of Conector
+     *
+     * @return mixed
+     */
+    public function getConector()
+    {
+        return $this->conector;
+    }
+
+    /**
+     * Set the value of Conector
+     *
+     * @param mixed conector
+     *
+     * @return self
+     */
+    public function setConector($conector)
+    {
+        $this->conector = $conector;
+
+        return $this;
+    }
+
+}
+?>
